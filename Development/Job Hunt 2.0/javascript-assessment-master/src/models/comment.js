2d424fea5cb9062c336a24f0b54627cc17dea346
Jordan Christley <lilj390150@live.com>
2018-06-07 12:17:11 -0500

    All class syntax should be in es6 now

--- Development/Job Hunt 2.0/javascript-assessment-master/src/models/comment.js
+++ Development/Job Hunt 2.0/javascript-assessment-master/src/models/comment.js
@@ -1,26 +1,22 @@
+'use strict'
 // create Comment class here
 class Comment {
     constructor(comment, imgId) {
+        Comment.all = [].push(this);
         this.id = Comment.all.length;
         this.commentContent = comment;
         this.img = this.findImage(imgId);
-        Comment.all.push(this);
     }
-    
-}
-
-Comment.all = [];
-
-Comment.prototype.findImage = function(id) {
-    var img = Image.all.filter(function(el) {
-        return el.id == id;
-    })[0];
-    img.comments.push(this);
-    return img;
-}
-
-Comment.prototype.commentEl = function() {
-    return `
-        <li id="comment-id-number-${this.id}">${this.commentContent}</li>
-    `
+    findImage(id) {
+        var img = Image.all.filter(function(el) {
+            return el.id == id;
+        })[0];
+        img.comments.push(this);
+        return img;
+    }
+    commentEl() {
+        return `
+            <li id="comment-id-number-${this.id}">${this.commentContent}</li>
+        `
+    }
 }
